361c4fdb223637c22f67d400ae1d716fd30269a0
Vinicius Vasconcelos Ferreira <vinicius.ferreira@synapsebrasil.com.br>
2021-12-27 14:40:42 -0300

    Mapeando N:N Produtos-Categorias

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,4 +13,9 @@ class Product extends Model
     {
         return $this->belongsTo(Store::class);
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
